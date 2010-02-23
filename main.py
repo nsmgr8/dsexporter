1fa0d32f722f3870ad2b7faa8de4ef377ff91727
M. Nasimul Haque <nasim.haque@gmail.com>
2010-02-23 19:18:07 +0000

    date time column added

--- main.py
+++ main.py
@@ -17,17 +17,36 @@ from google.appengine.ext.webapp import util
 from google.appengine.api import urlfetch, memcache
 from BeautifulSoup import BeautifulSoup
 
-dseurl = "http://www.dsebd.org/latest_share_price_all.php"
-dsesanere = re.compile('<body[^>]*>')
 
+dseroot = "http://www.dsebd.org/"
+dselatest = dseroot + "latest_share_price_all.php"
+dsesanere = re.compile(r'<body[^>]*>')
+datere = re.compile(r'[a-zA-Z]{3}\s*\d{2},\s*\d{4}\s*at\s*\d{2}:\d{2}:\d{2}')
+
+time_key = 'timekey'
 data_key = 'csvdata'
 cache_time = 10 * 60 # ten minutes
 
 class DSEHandler(webapp.RequestHandler):
 
+    def _get_time(self):
+        last_update = memcache.get(time_key)
+        if last_update:
+            return last_update
+
+        response = urlfetch.fetch(dseroot)
+        if response.status_code == 200:
+            last_update = datere.search(response.content).group()
+            last_update = last_update.replace(' ', '')
+            last_update = datetime.datetime.strptime(last_update, "%b%d,%Yat"
+                                                     "%H:%M:%S")
+            logging.info("Last update on %s" % (last_update))
+            memcache.set(time_key, last_update, cache_time)
+            return last_update
+
     def get(self):
-        now = datetime.datetime.now()
-        csvname = 'dse-%s.csv' % now.isoformat()
+        last_update = self._get_time()
+        csvname = 'dse-%s.csv' % last_update.isoformat()
 
         self.response.headers.add_header('content-disposition',
                                          'attachment', filename=csvname)
@@ -39,7 +58,7 @@ class DSEHandler(webapp.RequestHandler):
             logging.info('returning from cache')
             return
 
-        dseresult = urlfetch.fetch(dseurl)
+        dseresult = urlfetch.fetch(dselatest)
         if dseresult.status_code == 200:
             dsecontent = dseresult.content
             dsecontent = dsesanere.sub('<body>', dsecontent)
@@ -51,14 +70,16 @@ class DSEHandler(webapp.RequestHandler):
             csvfile = csv.writer(output)
             heads = []
             for h in headtr:
-                heads.append(str(h.contents[0]).replace('&nbsp;', ''))
+                heads.append(str(h.contents[0]).replace('&nbsp;', '').strip())
 
+            heads.insert(1, "Date Time")
             csvfile.writerow(heads)
 
             data = soup.body.table.findAll('tr')[1:]
             for row in data:
                 row = row.findAll('td')[1:]
                 d = [row[0].a.contents[0],]
+                d.append(last_update)
                 for col in row[1:]:
                     d.append(col.find(text=True))
 
